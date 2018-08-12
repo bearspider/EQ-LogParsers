5e9ff9f68de1f5d8304ec9b69b368ee4a113a563
bearspider <34553841+bearspider@users.noreply.github.com>
2018-08-12 18:06:58 -0500

    Add files via upload

--- eq-push-locator.py
+++ eq-push-locator.py
@@ -5,6 +5,7 @@ pushbackFile = 'pushback-list.csv'
 pushupFile = 'pushup-list.csv'
 
 logSpell = re.compile('\[(?P<EQTIME>\w+\s\w+\s+\d+\s\d+:\d+:\d+\s\d+)\]\s(?P<CHARACTER>\w+)\sbegins\sto\scast\sa\sspell\.\s\<(?P<SPELL>.+)\>')
+logSong = re.compile('\[(?P<EQTIME>\w+\s\w+\s+\d+\s\d+:\d+:\d+\s\d+)\]\s(?P<CHARACTER>\w+)\sbegin(s)\sto\ssing\sa\ssong\.\s\<(?P<SPELL>.+)\>')
 logfile = open(eqLogFile, 'r')
 pushupReader = open(pushupFile, 'r')
 pushbackReader = open(pushbackFile, 'r')
@@ -16,11 +17,17 @@ pushbackReader.close()
 
 while True:
   line = logfile.readline()
-  result = logSpell.search(line)
-  if result is not None:
-      if result.group('SPELL') in pushbackset:
-          print("{} PUSHBACK: {}".format(result.group('CHARACTER'),result.group('SPELL')))
-      if result.group('SPELL') in pushupset:
-          print("{} PUSHUP: {}".format(result.group('CHARACTER'),result.group('SPELL')))
+  spellresult = logSpell.search(line)
+  songresult = logSong.search(line)
+  if spellresult is not None:
+      if spellresult.group('SPELL') in pushbackset:
+          print("{} PUSHBACK: {}".format(spellresult.group('CHARACTER'),spellresult.group('SPELL')))
+      if spellresult.group('SPELL') in pushupset:
+          print("{} PUSHUP: {}".format(spellresult.group('CHARACTER'),spellresult.group('SPELL')))
+  if songresult is not None:
+      if songresult.group('SPELL') in pushbackset:
+          print("{} PUSHBACK: {}".format(songresult.group('CHARACTER'),songresult.group('SPELL')))
+      if songresult.group('SPELL') in pushupset:
+          print("{} PUSHUP: {}".format(songresult.group('CHARACTER'),songresult.group('SPELL')))
 
 logfile.close()
